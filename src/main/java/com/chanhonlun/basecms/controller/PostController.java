3928cecbba315ea4b3046eda9081a48d73eb7485
hlchanad <hlchanad@stu.ust.hk>
2018-09-19 18:36:48 +0800

    add controller trait for default /create routes

--- src/main/java/com/chanhonlun/basecms/controller/PostController.java
+++ src/main/java/com/chanhonlun/basecms/controller/PostController.java
@@ -1,10 +1,9 @@
 package com.chanhonlun.basecms.controller;
 
-import com.chanhonlun.basecms.constant.MyConstants;
+import com.chanhonlun.basecms.controller.trait.DefaultControllerHasCreatePageWithPojoDetail;
 import com.chanhonlun.basecms.controller.trait.DefaultControllerHasDataTable;
 import com.chanhonlun.basecms.controller.trait.DefaultControllerHasDeleteActionButton;
 import com.chanhonlun.basecms.controller.trait.DefaultControllerHasDetailPageWithPojoDetail;
-import com.chanhonlun.basecms.form.FormError;
 import com.chanhonlun.basecms.form.PostForm;
 import com.chanhonlun.basecms.pojo.Post;
 import com.chanhonlun.basecms.pojo.PostDetail;
@@ -12,26 +11,19 @@ import com.chanhonlun.basecms.response.vo.row.PostRowVO;
 import com.chanhonlun.basecms.service.page.BaseService;
 import com.chanhonlun.basecms.service.page.PostService;
 import com.chanhonlun.basecms.service.trait.DefaultServiceHasCRUD;
+import com.chanhonlun.basecms.service.trait.DefaultServiceHasCreatePageWithPojoDetail;
 import com.chanhonlun.basecms.service.trait.DefaultServiceHasDataTable;
 import com.chanhonlun.basecms.service.trait.DefaultServiceHasDetailPageWithPojoDetail;
-import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/post")
 public class PostController extends BaseController implements
         DefaultControllerHasDeleteActionButton<Post, Long>,
         DefaultControllerHasDataTable<Post, Long, PostRowVO>,
-//        DefaultControllerHasCreatePage,
+        DefaultControllerHasCreatePageWithPojoDetail<Post, Long, PostForm>,
         DefaultControllerHasDetailPageWithPojoDetail<Post, Long, PostDetail, Long> {
 
     @Autowired
@@ -57,24 +49,8 @@ public class PostController extends BaseController implements
         return postService;
     }
 
-    @GetMapping("/create")
-    public String create(Model model) {
-        model.addAttribute(MyConstants.PAGE_RESPONSE, postService.getCreatePageConfig());
-        return section + "/create";
+    @Override
+    public DefaultServiceHasCreatePageWithPojoDetail<Post, Long, PostForm> getDefaultPageHasCreate() {
+        return postService;
     }
-
-    @PostMapping("/create")
-    public String doCreate(Model model, PostForm form) {
-
-        FormError formError = postService.ifError(form);
-        if (formError != null) {
-            model.addAttribute(MyConstants.PAGE_RESPONSE, postService.getCreatePageConfig(form, formError));
-            return section + "/create";
-        }
-
-        Post post = postService.create(form);
-
-        return "redirect:/" + section + "/" + post.getId() + "/detail";
-    }
-
 }
---
+++ src/main/java/com/chanhonlun/basecms/controller/trait/DefaultControllerHasCreatePage.java
@@ -0,0 +1,42 @@
+package com.chanhonlun.basecms.controller.trait;
+
+import com.chanhonlun.basecms.constant.MyConstants;
+import com.chanhonlun.basecms.form.BaseForm;
+import com.chanhonlun.basecms.form.FormError;
+import com.chanhonlun.basecms.pojo.BasePojo;
+import com.chanhonlun.basecms.service.trait.DefaultServiceHasCreatePage;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.Serializable;
+
+public interface DefaultControllerHasCreatePage<
+        Pojo extends BasePojo<PojoPK>,
+        PojoPK extends Serializable,
+        Form extends BaseForm> {
+
+    String getSection();
+
+    DefaultServiceHasCreatePage<Pojo, PojoPK, Form> getDefaultPageHasCreate();
+
+    @GetMapping("/create")
+    default String create(Model model) {
+        model.addAttribute(MyConstants.PAGE_RESPONSE, getDefaultPageHasCreate().getCreatePageConfig());
+        return getSection() + "/create";
+    }
+
+    @PostMapping("/create")
+    default String doCreate(Model model, Form form) {
+
+        FormError formError = getDefaultPageHasCreate().ifError(form);
+        if (formError != null) {
+            model.addAttribute(MyConstants.PAGE_RESPONSE, getDefaultPageHasCreate().getCreatePageConfig(form, formError));
+            return getSection() + "/create";
+        }
+
+        Pojo post = getDefaultPageHasCreate().create(form);
+
+        return "redirect:/" + getSection() + "/" + post.getId() + "/detail";
+    }
+}
