46e3c74e546d64de1774453eb36ca2458b042d1f
hlchanad <hlchanad@stu.ust.hk>
2018-10-23 00:55:19 +0800

    add a custom voter for evaluating if role has permission on routes

--- src/main/java/com/chanhonlun/basecms/config/WebSecurityConfiguration.java
+++ src/main/java/com/chanhonlun/basecms/config/WebSecurityConfiguration.java
@@ -1,11 +1,17 @@
 package com.chanhonlun.basecms.config;
 
 import com.chanhonlun.basecms.security.matcher.AssetsMatcher;
+import com.chanhonlun.basecms.security.voter.RoleRoutePermissionVoter;
 import com.chanhonlun.basecms.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDecisionManager;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.vote.AuthenticatedVoter;
+import org.springframework.security.access.vote.RoleVoter;
+import org.springframework.security.access.vote.UnanimousBased;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,9 +19,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.expression.WebExpressionVoter;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -49,6 +57,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                     .requestMatchers(assetsMatcher).permitAll()
                     .antMatchers("/**").authenticated()
+                    .accessDecisionManager(accessDecisionManager())
                 .and()
                 .formLogin()
                     .loginPage("/login")
@@ -70,5 +79,15 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private AccessDecisionManager accessDecisionManager() {
+        List<AccessDecisionVoter<?>> decisionVoters = Arrays.asList(
+                new WebExpressionVoter(),
+                new RoleVoter(),
+                new AuthenticatedVoter(),
+                new RoleRoutePermissionVoter(contextPath, assetsMatcher)
+        );
+        return new UnanimousBased(decisionVoters);
     }
 }
---
+++ src/main/java/com/chanhonlun/basecms/security/voter/RoleRoutePermissionVoter.java
@@ -0,0 +1,56 @@
+package com.chanhonlun.basecms.security.voter;
+
+import com.chanhonlun.basecms.model.UserPrincipal;
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.AccessDecisionVoter;
+import org.springframework.security.access.ConfigAttribute;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.util.Collection;
+
+public class RoleRoutePermissionVoter implements AccessDecisionVoter<FilterInvocation> {
+
+    private static final Logger logger = LoggerFactory.getLogger(RoleRoutePermissionVoter.class);
+
+    private final String contextPath;
+    private final RequestMatcher assetsMatcher;
+
+    public RoleRoutePermissionVoter(String contextPath, RequestMatcher assetsMatcher) {
+        this.contextPath = contextPath;
+        this.assetsMatcher = assetsMatcher;
+    }
+
+    @Override
+    public int vote(Authentication authentication, FilterInvocation filterInvocation, Collection<ConfigAttribute> attributes) {
+
+        if (assetsMatcher.matches(filterInvocation.getHttpRequest())) return ACCESS_ABSTAIN;
+
+        if (!(authentication.getPrincipal() instanceof UserPrincipal)) return ACCESS_ABSTAIN;
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+
+        String method   = filterInvocation.getHttpRequest().getMethod();
+        String endpoint = filterInvocation.getHttpRequest().getRequestURI().substring(contextPath.length());
+
+        boolean allowed = userPrincipal.getAllowedRoutes()
+                .stream()
+                .anyMatch(roleRoute -> endpoint.startsWith(roleRoute.getUrl())
+                        && roleRoute.getMethod() == null || roleRoute.getMethod().matches(method));
+
+        return allowed ? ACCESS_GRANTED : ACCESS_DENIED;
+    }
+
+    @Override
+    public boolean supports(ConfigAttribute attribute) {
+        return true;
+    }
+
+    @Override
+    public boolean supports(Class clazz) {
+        return true;
+    }
+}
