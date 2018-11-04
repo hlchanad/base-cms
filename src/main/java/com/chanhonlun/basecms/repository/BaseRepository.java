6a226dc6fa3cf56be250df6f929d17f5990b9ea3
hlchanad <hlchanad@stu.ust.hk>
2018-11-05 00:59:20 +0800

    fix base repository generic type

--- src/main/java/com/chanhonlun/basecms/repository/BaseRepository.java
+++ src/main/java/com/chanhonlun/basecms/repository/BaseRepository.java
@@ -1,12 +1,13 @@
 package com.chanhonlun.basecms.repository;
 
+import com.chanhonlun.basecms.pojo.BasePojo;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
 @NoRepositoryBean
-public interface BaseRepository<Pojo, PK extends Serializable>
+public interface BaseRepository<Pojo extends BasePojo<PK>, PK extends Serializable>
         extends CrudRepository<Pojo, PK> {
 
     Pojo findByIdAndIsDeleteFalse(PK id);
